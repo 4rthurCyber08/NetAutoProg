fdc46d5b180d1d22dd6dfb553cf6f7dd4e363f19
ArJames Inson <aerin.james820@gmail.com>
2024-10-07 19:26:32 +0800

    Add files via upload

---
+++ NETPROG/csr_jsonPy/script/updateLib.py
@@ -0,0 +1,8 @@
+import sys
+print('version.sys')
+
+#type in the python terminal
+    #py -m pip install --upgrade pip
+    #py -m pip install netmiko
+    
+#if there are issues, type the commands in powershell as administrator 
