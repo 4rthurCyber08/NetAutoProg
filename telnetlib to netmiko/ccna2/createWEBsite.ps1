4faa1391d350b6191b1dc01e4f9a05f04404a295
ArJames Inson <aerin.james820@gmail.com>
2024-11-18 15:25:47 +0800

    Delete telnetlib to netmiko directory

--- telnetlib to netmiko/ccna2/createWEBsite.ps1
+++
@@ -1,2 +0,0 @@
-﻿Install-WindowsFeature  -name Web-Server  -includeManagementTools
-New-Website -name "ngcpM.ph" -hostheader "www.ngcpM.ph" -physicalpath "d:\webs\datingbiz"
--- telnetlib to netmiko/ccna2/dnsZoneTransfer.ps1
+++
@@ -1,2 +0,0 @@
-﻿Start-DnsServerZoneTransfer -name "ngcp12.ph" -FullTransfer
-#Add-DnsServerSecondaryZone -name "ngcpK.ph" -ZoneFile "ngcpK.ph.dns" -MasterServers 10.k.1.10
